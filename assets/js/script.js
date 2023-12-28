99044592d7ad6be96d0a4029c6ad556c04226c43
ImDanyul <>
2023-12-28 09:17:37 +0000

    add play game function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 let wins = 0;
 let losses = 0;
 let draws = 0;
+
+// Play Game Area
+
+function playGame(playerChoice) {
+
+    // Player Choice
+    const choices = ['rock', 'paper', 'scissors'];
+    // Computer Choice
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // Determine Winner + Score
+    const result = determineWinner(playerChoice, computerChoice);
+    updateScore(result)
+}
+
+// Determine Winner
